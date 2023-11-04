0f2599dec7b3e0f870bdf595a6c0e22ce7a80d59
ediaz23 <esteban.diazt23@gmail.com>
2023-11-04 11:11:24 -0300

    [IMP] Making customRequest more generic

--- src/hooks/customFetch.js
+++ src/hooks/customFetch.js
@@ -8,56 +8,104 @@ const { webOS } = window
 
 const serviceURL = 'luna://com.crunchyroll.stream.app.service/'
 
+
+/**
+ * Hack class to set url property
+ */
+class ResponseHack extends Response {
+    constructor(body, options) {
+        super(body, options)
+        if (options && options.url) {
+            this.originUrl = options.url
+        } else {
+            this.originUrl = ''
+        }
+    }
+    get url() {
+        return this.originUrl
+    }
+}
+
+
 /**
  * Function to bypass cors issues
- * @returns {Promise}
+ * @param {String} url
+ * @param {RequestInit} [options]
+ * @returns {Promise<Response>}
  */
-const customFetch = async (url, config) => {
+export const customFetch = async (url, options = {}) => {
     return new Promise((res, rej) => {
-        config.url = url
+        const config = { ...options }
         if (config.body && config.body instanceof URLSearchParams) {
             config.body = config.body.toString()
         }
         const onSuccess = (data) => {
-            logger.debug('res okey')
-            logger.debug(data)
-            const res2 = {
-                ok: true,
-                status: data.status,
-                statusText: data.statusText,
-                json: async () => Promise.resolve(JSON.parse(data.content)),
-                text: async () => Promise.resolve(data.content),
+            const { status, statusText, content, headers } = data
+            const binaryString = atob(content)
+            const bytes = new Uint8Array(binaryString.length)
+
+            for (let i = 0; i < binaryString.length; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
             }
-            if (200 <= data.status && data.status < 300) {
-                res(res2)
-            } else {
-                res2.ok = false
-                res(res2)
-            }
+            logger.debug(`res okey ${status} ${url}`)
+            res(new ResponseHack(new window.Blob([bytes]), {
+                status,
+                statusText,
+                headers,
+                url,
+            }))
         }
         const onFailure = (error) => {
             logger.error('res error')
             logger.error(error)
             rej(error)
         }
+        logger.debug(`${config.method} ${url}`)
         if (utils.isTv()) {
             webOS.service.request(serviceURL, {
                 method: 'forwardRequest',
-                parameters: config,
+                parameters: { url, ...config },
                 onSuccess,
                 onFailure
             })
         } else {
-            fetch('http://localhost:8052/webos2', {
+            window.fetchBak('http://localhost:8052/webos2', {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(config),
+                body: JSON.stringify({ url, ...config }),
             }).then(res2 => res2.json()).then(onSuccess).catch(onFailure)
         }
     })
 }
+
+/**
+ * Function to bypass cors issues
+ * @param {String} url
+ * @param {RequestInit} [options]
+ * @returns {Promise<Response>}
+ */
+const replaceFetch = async (url, options = {}) => {
+    if (url instanceof Request) {
+        if (url.headers.get('is-front-hls') === 'true') {
+            url.headers.delete('is-front-hls')
+            const { method, body, headers } = url
+            return customFetch(url.url, {
+                method: method || 'get',
+                body,
+                headers
+            })
+        }
+    }
+    return window.fetchBak(url, options)
+}
+
+if (!window.fetchBak) {
+    window.fetchBak = window.fetch
+    window.fetch = replaceFetch
+}
+
 
 const useCustomFetch = () => customFetch
 
