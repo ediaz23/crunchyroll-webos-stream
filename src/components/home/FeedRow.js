5f71b76230a3b005b4e2b64709b3c61a224426c6
ediaz23 <esteban.diazt23@gmail.com>
2023-12-13 21:09:24 -0300

    [IMP] Playing musicVideo musicConcerts

--- src/components/home/FeedRow.js
+++ src/components/home/FeedRow.js
@@ -93,7 +93,7 @@ const HomeFeedRow = ({ feed, itemSize, cellId, setContent, style, className, ind
     /** @type {Function} */
     const doSelectElement = useCallback(content => {
         back.pushHistory({ doBack: () => { setPath('/profiles/home') } })
-        if (['episode', 'musicConcert', 'movie'].includes(content.type)) {
+        if (['episode', 'musicConcert', 'movie', 'musicVideo'].includes(content.type)) {
             if (content.type === 'movie' && content.panel) {
                 setPlayContent({ ...content, ...content.panel, panel: null })
             } else {
