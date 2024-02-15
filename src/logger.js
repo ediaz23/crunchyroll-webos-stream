87b6163697a7371283ec39f3a9d97805f9650bef
ediaz23 <esteban.diazt23@gmail.com>
2024-02-15 11:57:47 -0300

    [IMP] Disable color on TV

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,11 @@
 
 import { logger } from 'crunchyroll-js-api'
+import utils from './utils'
 
 logger.setLevel('debug')
+
+if (utils.isTv()) {
+    logger.activeColor(false)
+}
 
 export default logger
