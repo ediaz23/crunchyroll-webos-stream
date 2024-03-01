9b3f7ef7d99f844544b9617c510cb0f2e48008e7
ediaz23 <esteban.diazt23@gmail.com>
2024-02-29 21:29:25 -0300

    [IMP] Visual details

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,11 @@
 import { logger } from 'crunchyroll-js-api'
 import utils from './utils'
 
-logger.setLevel('debug')
+if (process.env.NODE_ENV !== 'production') {
+    logger.setLevel('debug')
+} else {
+    logger.setLevel('error')
+}
 
 if (utils.isTv()) {
     logger.activeColor(false)
