8a92470b4d48bdc43f0edf1ccfaedd53263000d3
ediaz23 <esteban.diazt23@gmail.com>
2024-04-29 23:21:41 -0400

    [FIX] Audio list

--- src/api/misc.js
+++ src/api/misc.js
@@ -1,25 +1,7 @@
 
-import { localStore, config } from 'crunchyroll-js-api'
+import { localStore } from 'crunchyroll-js-api'
 import { customFetch } from '../hooks/customFetch'
 
-/**
- * Return audio lang list supported
- * @returns {Promise<Array<String>>}
- */
-export const getAudioLangList = async () => config.i18n.audio_languages
-
-/**
- * Return subtitles lang list supported
- * @returns {Promise<Array<String>>}
- */
-export const getSubtitleLangList = async () => ['off', ...config.i18n.text_languages]
-
-
-/**
- * Return content lang list supported
- * @returns {Promise<Array<String>>}
- */
-export const getContentLangList = async () => config.i18n.supported
 
 /**
  * Expand a sort url
@@ -31,3 +13,43 @@ export const fetchAuth = async (url) => {
     return customFetch(url, { headers: { Autorization: token } })
 }
 
+
+/**
+ * Return audio lang list supported
+ * @returns {Promise<Array<String>>}
+ */
+export const getAudioLangList = async () => {
+    const res = await fetchAuth('https://static.crunchyroll.com/config/i18n/v3/audio_languages.json')
+    const data = await res.json()
+    return ["ja-JP", ...Object.keys(data)]
+}
+
+
+/**
+ * Return subtitles lang list supported
+ * @returns {Promise<Array<String>>}
+ */
+export const getSubtitleLangList = async () => {
+    const res = await fetchAuth('https://static.crunchyroll.com/config/i18n/v3/timed_text_languages.json')
+    const data = await res.json()
+    return ['off', ...Object.keys(data)]
+}
+
+
+/**
+ * Return content lang list supported
+ * @returns {Promise<Array<String>>}
+ */
+export const getContentLangList = async () => [
+    "ar-SA",
+    "de-DE",
+    "en-US",
+    "es-419",
+    "es-ES",
+    "fr-FR",
+    "it-IT",
+    "pt-BR",
+    "pt-PT",
+    "ru-RU",
+    "hi-IN"
+]
