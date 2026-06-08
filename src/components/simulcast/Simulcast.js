f03742500f2081fb901838dece882089874ee807
ediaz23 <esteban.diazt23@gmail.com>
2026-06-07 20:08:07 -0400

    [FIX] wrong simulcast

--- src/components/simulcast/Simulcast.js
+++ src/components/simulcast/Simulcast.js
@@ -23,6 +23,38 @@ import { sortContent } from '../../utils'
  * @property {{title: String, description: String}} localization
  */
 
+/**
+ * @param {Date} [date=new Date()]
+ * @returns {String}
+ */
+function getLocalSeason (date = new Date()) {
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    let out = 'spring'
+
+    if (
+        (month === 12 && day >= 21) ||
+        month < 3 ||
+        (month === 3 && day < 21)
+    ) {
+        return 'summer'
+    } else if (
+        (month === 3 && day >= 21) ||
+        month < 6 ||
+        (month === 6 && day < 21)
+    ) {
+        out = 'fall'
+    } else if (
+        (month === 6 && day >= 21) ||
+        month < 9 ||
+        (month === 9 && day < 23)
+    ) {
+        out = 'winter'
+    }
+
+    return out
+}
+
 
 /**
  * Simulcast view
@@ -102,7 +134,11 @@ const Simulcast = ({ profile, title, ...rest }) => {
             api.discover.getSeasonList(profile).then(({ data: seasonsList }) => {
                 seasonsList.forEach((item, index) => { item.index = index })
                 setSeasons(seasonsList)
-                setSeason(seasonsList[0])
+                if (seasonsList[0].id.split('-')[0] === getLocalSeason()) {
+                    setSeason(seasonsList[0])
+                } else {
+                    setSeason(seasonsList[1])
+                }
             })
         }
     }, [profile, setSeason, delay, season])
